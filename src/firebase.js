152b8c9685ccdb2a378e1ae9b5ed39803b2f3b10
uunegbu <donuzo84@yahoo.co.uk>
2022-03-12 03:42:32 +0100

    commit on 'master'

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQFWhYe11gNjeF5oybM1Wuv_a4De0LZVs",
+  authDomain: "delbottshop.firebaseapp.com",
+  projectId: "delbottshop",
+  storageBucket: "delbottshop.appspot.com",
+  messagingSenderId: "994642487665",
+  appId: "1:994642487665:web:bc51b2f0d69c8163f5aed0",
+  measurementId: "G-YN8KRH3MD3",
+};
