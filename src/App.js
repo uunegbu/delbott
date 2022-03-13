0a2f0dbba0c36734352d53c8110a1c89565d0032
uunegbu <donuzo84@yahoo.co.uk>
2022-03-14 00:22:25 +0100

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./Header.js";
+import Home from "./Home.js";
+import Product from "./Product.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
