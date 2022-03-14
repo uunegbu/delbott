dae78b58e4ffc6365985890a92c7946a86c6773d
uunegbu <donuzo84@yahoo.co.uk>
2022-03-14 01:13:43 +0100

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
 import Product from "./Product.js";
-
+import Checkout from "./Checkout.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
